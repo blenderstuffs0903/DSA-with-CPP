147e93b2c93ead7e1da3ae14b1b4bd416635d6d1
codestuffs <138683592+blenderstuffs0903@users.noreply.github.com>
2024-10-20 17:01:56 +0530

    optimization

--- DSA/9_Highest_Profit.cpp
+++ DSA/9_Highest_Profit.cpp
@@ -3,30 +3,38 @@
 using namespace std;
 
 int highest_profit(vector <int> prices) {
-    int SP = prices[0], BP = prices[0];
+    // int SP = prices[0], BP = prices[0];
     int buying_day = 0, selling_day = 0, profit = 0;
     int size = prices.size();
 
     for (int i = 1; i < size; i++) {
-        if (prices[i] - BP > profit) {
+        if (prices[i] - prices[buying_day] > profit) {
             selling_day = i;
-            SP = prices[i];
-            profit = SP - BP;
-        } else if (prices[i] < BP) {
+            profit = prices[i] - prices[buying_day];
+        } else if (prices[i] < buying_day) {
             buying_day = i;
-            BP = prices[i];
-            if (buying_day > selling_day) {
-                selling_day = buying_day;
-                SP = BP;
-            }
+            if (buying_day > selling_day) selling_day = buying_day;
         }
     }
     return profit;
+}
+// Simplified
+int max_profit(vector <int> prices) {
+    int best_buy{prices[0]};
+    int max_profit{0};
+
+    for (unsigned int i = 1; i < prices.size(); i++) {
+        if (prices[i] > best_buy)
+            max_profit = max(max_profit, prices[i] - best_buy);
+        best_buy = min(prices[i], best_buy);
+    }
+    return max_profit;
 }
 
 int main() {
     // vector <int> prices {7, 3, 5, 1, 6, 4};
     vector <int> prices {2, 4, 1};
     cout << highest_profit(prices) << endl;
+    cout << max_profit(prices) << endl;
     return 0;
 }
